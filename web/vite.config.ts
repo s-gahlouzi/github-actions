bb75c683e869b79111bed22a259bb2a51d42686d
Saif Eddine Gahlouzi <s.gahlouzi@instadeep.com>
2025-05-13 15:23:53 +0100

    add gtest coverage

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+		globals: true,
+		reporters: ['default'],
+		coverage: {
+			provider: 'istanbul',
+			reporter: ['text', 'html', 'cobertura'],
+			include: ['src/**/*.{ts,tsx}'],
+		},
+		testTimeout: 60000,
+		watch: false,
+		silent: true,
+		// to prevent this issue https://vitest.dev/guide/common-errors.html#failed-to-terminate-worker
+		pool: 'forks',
+	},
 })
